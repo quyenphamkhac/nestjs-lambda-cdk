140c0c88fd65590cf095b49e80625284fc6fed55
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2022-12-09 17:14:28 +0700

    Initial commit

---
+++ test/nestjs-lambda-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NestjsLambdaCdk from '../lib/nestjs-lambda-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nestjs-lambda-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NestjsLambdaCdk.NestjsLambdaCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
